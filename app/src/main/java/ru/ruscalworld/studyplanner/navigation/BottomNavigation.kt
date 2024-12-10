5e8302a15fbd2b026676c2ed2c5b785f58e712cd
ruscalworld <me@ruscal.world>
2024-12-10 17:39:28 +0300

    Configure navigation items

--- app/src/main/java/ru/ruscalworld/studyplanner/navigation/BottomNavigation.kt
+++ app/src/main/java/ru/ruscalworld/studyplanner/navigation/BottomNavigation.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeGesturesPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
@@ -12,12 +13,21 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.compose.currentBackStackEntryAsState
+import ru.ruscalworld.studyplanner.R
+import ru.ruscalworld.studyplanner.ui.theme.PrimaryColor
+import ru.ruscalworld.studyplanner.ui.theme.SecondaryText
 
 @Composable
 fun BottomNavigation(navController: NavHostController) {
@@ -28,6 +38,9 @@ fun BottomNavigation(navController: NavHostController) {
 
         launchSingleTop = true
     }
+
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStackEntry?.destination?.route
 
     BottomAppBar(
         modifier = Modifier
@@ -41,18 +54,46 @@ fun BottomNavigation(navController: NavHostController) {
                 horizontalArrangement = Arrangement.SpaceAround
             ) {
                 IconButton(onClick = { navController.navigate("diary/home", navOptions) }) {
-                    Icon(Icons.Filled.Call, contentDescription = "Test")
+                    Icon(
+                        painter = painterResource(R.drawable.fa_book_solid),
+                        contentDescription = stringResource(R.string.navigation_section_diary),
+                        tint = navSectionColor(currentDestination, "diary"),
+                        modifier = Modifier.size(24.dp),
+                    )
                 }
+
                 IconButton(onClick = { navController.navigate("stats/home", navOptions) }) {
-                    Icon(Icons.Filled.Call, contentDescription = "Test")
+                    Icon(
+                        painter = painterResource(R.drawable.fa_chart_simple_solid),
+                        contentDescription = stringResource(R.string.navigation_section_stats),
+                        tint = navSectionColor(currentDestination, "stats"),
+                        modifier = Modifier.size(24.dp),
+                    )
                 }
+
                 IconButton(onClick = { navController.navigate("editor/curriculums/@current", navOptions) }) {
-                    Icon(Icons.Filled.Call, contentDescription = "Test")
+                    Icon(
+                        painter = painterResource(R.drawable.fa_pencil_solid),
+                        contentDescription = stringResource(R.string.navigation_section_editor),
+                        tint = navSectionColor(currentDestination, "editor"),
+                        modifier = Modifier.size(24.dp),
+                    )
                 }
+
                 IconButton(onClick = { navController.navigate("options/info", navOptions) }) {
-                    Icon(Icons.Filled.Call, contentDescription = "Test")
+                    Icon(
+                        painter = painterResource(R.drawable.fa_bars_solid),
+                        contentDescription = stringResource(R.string.navigation_section_options),
+                        tint = navSectionColor(currentDestination, "options"),
+                        modifier = Modifier.size(24.dp),
+                    )
                 }
             }
         }
     )
+}
+
+fun navSectionColor(currentDestination: String?, targetPrefix: String): Color {
+    if (currentDestination == null || !currentDestination.startsWith(targetPrefix)) return SecondaryText
+    return PrimaryColor
 }
